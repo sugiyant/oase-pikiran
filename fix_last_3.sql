034aaddd84236813c85e9b4bff29170a004b81ec
Sugiyanto <giyanto77@gmail.com>
2026-09-13 08:47:53 +0700

    feat: move bookmark API endpoints to Astro SSR routes

--- fix_last_3.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM articles WHERE slug = 'su-sup-o-cua-cac-nen-van-minh-duoi-goc-nhin-entropy';
-DELETE FROM articles WHERE slug = 'kinh-te-hoc-chu-y-xay-dung-hang-rao-nhan-thuc-chong-lai-su-bao-hoa-thong-tin';
-DELETE FROM articles WHERE slug = 'su-im-lang-cua-cai-toi-giai-ma-nghich-ly-huy-diet-ban-nga';
--- run-translation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -a
-source ~/.env
-set +a
-node scripts/translate-vietnamese.js > translate.log 2>&1
